ad90bc14fc2d09daad1b5702a562186a69354754
Kamlesh Yadav <kamleshyadav875@gmail.com>
2025-11-11 16:50:58 +0530

    Initial Project upload

---
+++ src/main/java/com/ecom/cartify/UserService.java
@@ -0,0 +1,23 @@
+package com.ecom.cartify;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private List<User> userList = new ArrayList<>();
+
+    public List<User> fetchAllUsers(){
+        return userList;
+    }
+
+    public List<User> addUser(User user){
+        userList.add(user);
+        return userList;
+    }
+
+
+}
