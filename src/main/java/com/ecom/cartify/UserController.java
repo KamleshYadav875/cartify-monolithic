b1cbed39b35087b7c8dd8201a733e6fd7d525330
Kamlesh Yadav <kamleshyadav875@gmail.com>
2025-11-12 16:59:44 +0530

    Added user

--- src/main/java/com/ecom/cartify/UserController.java
+++ src/main/java/com/ecom/cartify/UserController.java
@@ -2,10 +2,10 @@ package com.ecom.cartify;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,13 +18,33 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/api/users")
-    public List<User> getAllUsers(){
-        return userService.fetchAllUsers();
+    public ResponseEntity<List<User>> getAllUsers(){
+        return new ResponseEntity<>(userService.fetchAllUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/api/users/{id}")
+    public ResponseEntity<User> getUsers(@PathVariable Long id){
+//        User user  = userService.fetchUsers(id);
+//        if(ObjectUtils.isEmpty(user)){
+//            return ResponseEntity.notFound().build();
+//        }
+//        return ResponseEntity.ok(user);
+
+        return userService.fetchUsers(id).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping("/api/users")
-    public String createUsers(@RequestBody User user){
+    public ResponseEntity<String> createUsers(@RequestBody User user){
         userService.addUser(user);
-        return "User added successfully";
+        return ResponseEntity.ok("User added successfully");
+    }
+
+    @PutMapping("/api/users/{id}")
+    public ResponseEntity<String> updateUsers(@PathVariable Long id, @RequestBody User user){
+        boolean updated = userService.updateUser(id, user);
+        if(updated){
+            return ResponseEntity.ok("User updated successfully");
+        }
+        return ResponseEntity.notFound().build();
     }
 }
